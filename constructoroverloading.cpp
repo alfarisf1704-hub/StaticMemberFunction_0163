f21fad7b9d63fb664d61453d4c48f60f89183b94
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-04 15:54:40 +0700

    membuat int main nya

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -12,8 +12,8 @@ public:
     mahasiswa();
     mahasiswa(int);
     mahasiswa(string);
-mahasiswa(int iNim, string iNama;
-void cetak();
+    mahasiswa(int iNim, string iNama);
+    void cetak();
 };
 
 mahasiswa ::mahasiswa()
@@ -40,4 +40,21 @@ void mahasiswa ::cetak()
 {
     cout << endl
          << "Nim=" << nim << endl;
+    cout << "Nama" << nama << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    cout << "Mahasiswa 1";
+    mhs1.cetak();
+    cout << "Mahasiswa 2";
+    mhs2.cetak();
+    cout << "Mahasiswa 3";
+    mhs3.cetak();
+
+    return 0;
 }
