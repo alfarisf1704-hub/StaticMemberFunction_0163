7afcab7a36170c68258db71c2e263b662c9ccd6e
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-04 16:11:52 +0700

    void mahasiswa set id print all, dan in main

--- staticmember.cpp
+++ staticmember.cpp
@@ -14,4 +14,33 @@ public:
     {
         setID();
     }
+};
+
+int mahasiswa::nim = 10;
+
+void mahasiswa::setID()
+{
+    id = ++nim;
+};
+
+void mahasiswa::printALL()
+{
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
+
+int main()
+{
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    return 0;
 }
