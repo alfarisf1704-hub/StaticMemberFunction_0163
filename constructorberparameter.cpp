735db8f3e9d2548bfa5a29aed0c986e715a9ef14
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-04 16:33:05 +0700

    class mahasiswa construc berparameter

---
+++ constructorberparameter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    mahasiswa(int nim, string nama);
+};
+
+mahasiswa::mahasiswa(int nim, string nama)
+{
+    cout << "constructor dengan parameter terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
